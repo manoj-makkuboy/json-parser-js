9cb45634af82b8a4b4436a00bdbd685a94576e98
manoj-makkuboy <m.manoj@linuxmail.org>
2018-06-26 12:59:05 +0530

    Add boolean parser

--- jsonParser.js
+++ jsonParser.js
@@ -1,5 +1,20 @@
 exports.parsers = {
-  nullParser: function nullParser() {
-    console.log('hello null parser')
+
+  booleanParser: function booleanParser(toBeParsed) {
+    let parsed, unParsed
+
+    if (toBeParsed.match(/^true/) !== null) {
+      parsed = true
+      unParsed = toBeParsed.replace(/^true/, '')
+    }
+
+    if (toBeParsed.match(/^false/) !== null) {
+      parsed = false
+      unParsed = toBeParsed.replace(/^false/, '')
+    }
+
+    return {
+      parsed, unParsed
+    }
   }
 }
