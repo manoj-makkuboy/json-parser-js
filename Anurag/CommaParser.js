09287a5ad3c1a52b96fe1d4aeecee3299474b5fe
Anurag <anuragtomar@railsfactory.org>
2018-06-27 12:40:04 +0530

    Changes made in Comma Parser

--- Anurag/CommaParser.js
+++ Anurag/CommaParser.js
@@ -2,11 +2,9 @@ exports.cParser = {
 	commaParser: function commaParser(dataToParse) {
 		console.log("Comma Parser by Anurag")
     	console.log("Data Passed = " + dataToParse);
-    	var unparsed;
-    	unparsed = dataToParse.split(",");
-    	for (var i = 0; i < unparsed.length; i++) {
-    		console.log(unparsed[i]);
-    	}
-    	return unparsed;
+        if (dataToParse.startsWith(",")) {
+            return [null,dataToParse.slice(1)];
+        }
+    	return [null,dataToParse];
 	}
 }
