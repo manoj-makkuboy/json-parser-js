89bf942d6646e34d7c92e22401837d16138a998b
trishita <trishita@railsfactory.org>
2018-06-27 14:33:27 +0530

    arrayParser implemented along with boolean and comma parser

--- trishita/commaParser.js
+++ trishita/commaParser.js
@@ -1,7 +1,10 @@
-let commaParser = (toBeParsedString) => {
-  if (toBeParsedString.startsWith(",")) {
-    return [null, toBeParsedString.slice(1)];
-  } else {
-    return [null, toBeParsedString];
+exports.commaParser = {
+  commaParser: toBeParsedString => {
+    if (toBeParsedString.startsWith(",")) {
+      return [null, toBeParsedString.slice(1)];
+    } else {
+      return [null, toBeParsedString];
+    }
   }
 }
+
