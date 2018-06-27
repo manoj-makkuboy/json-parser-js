ee656d615336dc3e68819a5f8da5ab3fc464c297
trishita <trishita@railsfactory.org>
2018-06-27 10:02:44 +0530

    created Array and comma parser

---
+++ trishita/commaParser.js
@@ -0,0 +1,7 @@
+let commaParser = (toBeParsedString) => {
+  if (toBeParsedString.startsWith(",")) {
+    return [null, toBeParsedString.slice(1)];
+  } else {
+    return [null, toBeParsedString];
+  }
+}
